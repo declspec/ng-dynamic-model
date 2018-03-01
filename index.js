44b861907c660265cbb95a7bbd96815f14cd5b17
Jason Larke <jason.larke@curtin.edu.au>
2018-03-01 14:58:07 +0800

    Removed commented out code from index?

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
 const lib = require('./bin/ng-dynamic-model');
 module.exports = lib;
-/*
-import { default as lib } from './src/ng';
-import * as util from './src/util';
-
-export * from './src/model';
-export * from './src/model-field';
-export { util };
-
-export default lib;*/
